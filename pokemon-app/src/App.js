c07b0032cd259020a8c603cd4ffdfffb7e8bee5f
Nobutaka Muranaka <chanayataka2324@gmail.com>
2024-04-12 07:59:13 +0900

    add：データ読み込み時のローディング設定
    useStateによるstateの変化を学習。初期値を設定し、処理が終わったところでstateの変更を設定。また三項演算子によるビューの切り替えも学んだ。

--- pokemon-app/src/App.js
+++ pokemon-app/src/App.js
@@ -1,21 +1,31 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import { getAllPokemon } from './utils/pokemon/pokemon.js';
 
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon/";
+  const [loading, setLoading] = useState(true); //初期値はtrue
 
   useEffect(() => {
     const fetchPokemonData = async () => {
       //全てのポケモンデータを取得
       let res = await getAllPokemon(initialURL);
       console.log(res);
+      setLoading(false); //取得ができてローディングが完了したのでstateを変更
     }
     fetchPokemonData();
   }, []);
 
   return (
-    <div className="App"></div>
+    <div className="App">
+      {loading ? (
+        <h1>ロード中・・・</h1>
+      ) : (
+        <>
+          <h1>ポケモンデータを取得しました</h1>
+        </>
+      )}
+    </div>
   );
 }
 
