08510a0a130597714bc58a2022b0cdbfeb88cdea
Nobutaka Muranaka <chanayataka2324@gmail.com>
2024-04-13 18:54:42 +0900

    ポケモンの詳細なデータを取得

--- pokemon-app/src/App.js
+++ pokemon-app/src/App.js
@@ -1,24 +1,38 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-import { getAllPokemon } from './utils/pokemon/pokemon.js';
+import { getAllPokemon, getPokemon } from './utils/pokemon/pokemon.js';
 
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon/";
   const [loading, setLoading] = useState(true); //初期値はtrue
+  const [pokemonData, setPokemonData] = useState([]);
 
   useEffect(() => {
     const fetchPokemonData = async () => {
       //全てのポケモンデータを取得
       let res = await getAllPokemon(initialURL);
-      console.log(res);
+      //各ポケモンの詳細なデータを取得
+      loadPokemon(res.results);
       setLoading(false); //取得ができてローディングが完了したのでstateを変更
     }
     fetchPokemonData();
   }, []);
 
+  const loadPokemon = async (data) => {
+    let _pokemonData =  await Promise.all(
+      data.map((pokemon) => {
+        let pokemonRecord = getPokemon(pokemon.url);
+        return pokemonRecord;
+      })
+    );
+    setPokemonData(_pokemonData);
+  };
+
+  console.log(pokemonData);
+
   return (
     <div className="App">
-      {loading ? (
+      {loading ? ( // 三項演算子でロード中かどうかを表示切り替え
         <h1>ロード中・・・</h1>
       ) : (
         <>
--- pokemon-app/src/utils/pokemon/pokemon.js
+++ pokemon-app/src/utils/pokemon/pokemon.js
@@ -5,3 +5,17 @@ export const getAllPokemon = (url) => {
             .then((data) => resolve(data));
     });
 };
+
+export const getPokemon = (url) => {
+    return new Promise((resolve, reject) => {
+        fetch(url)
+            .then((res) => res.json())
+            .then((data) => resolve(data));
+    });
+};
+
+// Promise（非同期処理）についてメモ
+// API取得やDB接続は開発者やユーザーでは保証できない
+// 成功（resolve）と失敗（reject）の2パターンを想定する必要がある
+// thenメソッドに処理内容を書く（成功時の非同期処理になっている）
+// catchメソッドにはエラー処理を書く（失敗時の非同期処理になっている）
